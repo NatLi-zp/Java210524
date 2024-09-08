eb6080a86287e00289f1d0a17edc46abcd0079f4
NatLi-zp <natali.gordienko@gmail.com>
2024-09-08 20:13:12 +0200

    Homework Java 080924 (enum) task 1

--- src/main/java/de/telran/lesson_4/homework_040924/task1/CalculatorInterface1.java
+++ src/main/java/de/telran/lesson_4/homework_040924/task1/CalculatorInterface1.java
@@ -1,40 +1,24 @@
 package de.telran.lesson_4.homework_040924.task1;
 
-//public class CalculatorInterface1<T> implements Calculator<T>{
-//    private T num1;
-//    private T num2;
+public class CalculatorInterface1 implements Calculator<Integer> {
 
-//public class CalculatorInterface1 implements Calculator{
-//    public int num1;
-//    public int num2;
+    @Override
+    public Integer add(Integer num1, Integer num2) {
+        return num1 + num2;
+    }
 
-//    public CalculatorInterface1(2) {
-//        this.num1 = num1;
-//        this.num2 = num2;
-//    }
+    @Override
+    public Integer sub(Integer num1, Integer num2) {
+        return  num1 - num2;
+    }
 
-//    @Override
-//    public <T> add(T num1, T num2) {
-//        return num1+num2;
-//    }
+    @Override
+    public Integer multi(Integer num1, Integer num2) {
+        return  num1 * num2;
+    }
 
-    //    @Override
-//    public T add() {
-//        return num1 + num2;
-//    }
-//
-//    @Override
-//    public T sub(T num1, T num2) {
-//        return null;
-//    }
-//
-//    @Override
-//    public T multi(T num1, T num2) {
-//        return null;
-//    }
-//
-//    @Override
-//    public T div(T num1, T num2) {
-//        return null;
-//    }
-//}
+    @Override
+    public Integer div(Integer num1, Integer num2) {
+        return  num2 != 0 ? num1 / num2 : 0;
+    }
+}
