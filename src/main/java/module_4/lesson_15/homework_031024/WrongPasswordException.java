2e93f49cecfc0682650464ce0151c9cdd7c26d09
NatLi-zp <natali.gordienko@gmail.com>
2024-10-03 19:55:49 +0200

    Homework Java 031024 (Exception)

---
+++ src/main/java/module_4/lesson_15/homework_031024/WrongPasswordException.java
@@ -0,0 +1,14 @@
+package module_4.lesson_15.homework_031024;
+
+public class WrongPasswordException extends RuntimeException{
+    private int codeException;
+
+    public WrongPasswordException() {
+        super("password и confirmPassword должны быть равны! ");
+    }
+
+    public WrongPasswordException(String message, int codeException) {
+        super(message);
+        this.codeException = codeException;
+    }
+}
